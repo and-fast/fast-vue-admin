9a89c75f506feb8abf7b45786bad14ae3226c7cf
ljd <helloxiangsheng@163.com>
2019-12-27 10:41:13 +0800

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fast-vue-admin
 vue后台管理系统模板
+
+# 演示
+<div align=center>
+    <img src="./desc/home.png" />
+</div>
--- src/components/layout/breadcrumb.vue
+++ src/components/layout/breadcrumb.vue
@@ -1,8 +1,8 @@
 <template>
   <el-breadcrumb separator="/">
     <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item :to="{ path: '/' }">活动列表</el-breadcrumb-item>
-    <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    <el-breadcrumb-item :to="{ path: '/' }">商品管理</el-breadcrumb-item>
+    <el-breadcrumb-item>商品列表</el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
